c8ec9ba1b247fc09f0f764def3e61876cba51490
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2023-11-30 17:25:23 +0100

    Make fields mandatory

--- src/controllers/send-report.js
+++ src/controllers/send-report.js
@@ -1,7 +1,29 @@
 import sanitizeHtml from "sanitize-html";
 
+function checkNestedProperty(obj, key) {
+	const keys = key.split(".");
+	return keys.every((k) => obj && (obj = obj[k]) !== undefined);
+}
+
 async function sendReport(payload) {
 	let { req, res, env } = payload;
+
+	const mandatoryFields = [
+		{ key: "email", message: "Email is required" },
+		{ key: "platform", message: "Platform is required" },
+		{ key: "platform.name", message: "Platform name is required" },
+		{ key: "platform.version", message: "Platform version is required" },
+		{ key: "platform.os", message: "Platform OS is required" },
+		{ key: "url", message: "URL is required" },
+		{ key: "issue", message: "Issue is required" },
+	];
+
+	for (const field of mandatoryFields) {
+		if (!checkNestedProperty(req.body, field.key)) {
+			res.status(500).send(field.message);
+			return;
+		}
+	}
 
 	let issue = sanitizeHtml(req.body.issue);
 
